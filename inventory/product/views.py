efaa29cbf53b52d62f3bdfbb1e6ab08420a135c3
Hariharan Achary <hariachary7@gmail.com>
2024-10-24 16:30:43 +0530

    Category Listing Added

--- inventory/product/views.py
+++ inventory/product/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
 from common.utils import Authentication
+from .models import *
+
+
+@Authentication.inventory_login_decorator
+def inventory_add_category(request):
+    try:
+        context = {}
+        context['active_menu_item'] = 'Add Category'
+
+        category_list = model_get_category_list()
+        if not category_list:
+            context['category_list_err'] = 'No Category Avaliable'
+            return render(request, 'inventory/view_inventory_add_category.html', context)
+
+        res_category = []
+        for category_item in category_list:
+            category = {}
+            category['category_id'] = category_item['category_id']
+            category['category_name'] = category_item['category_name']
+            category['category_description'] = category_item['category_description']
+            category['category_product_count'] = category_item['product_count']
+
+            res_category.append(category)
+        
+        context['category_list'] = res_category
+        
+        return render(request, 'inventory/view_inventory_add_category.html', context)
+
+    except Exception as ex:
+        print(ex)
 
 
 @Authentication.inventory_login_decorator
@@ -9,17 +39,6 @@ def inventory_add_products(request):
         context['active_menu_item'] = 'Add Product'
         
         return render(request, 'inventory/view_inventory_add_products.html', context)
-
-    except Exception as ex:
-        print(ex)
-
-@Authentication.inventory_login_decorator
-def inventory_add_category(request):
-    try:
-        context = {}
-        context['active_menu_item'] = 'Add Category'
-        
-        return render(request, 'inventory/view_inventory_add_category.html', context)
 
     except Exception as ex:
         print(ex)
